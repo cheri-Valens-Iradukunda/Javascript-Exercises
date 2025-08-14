17b46c9eed592b2a6240512ffb40da6cf060a0d7
cheri_Valens_Iradukunda <iradukundacherivalens88@gmail.com>
2025-08-14 10:36:00 +0200

    adding addition excluding min and max kata

---
+++ Codewars/AddingNumbers.js
@@ -0,0 +1,10 @@
+function sumArray(array) {
+  if(array.length < 2) return 0
+  array.splice(array.indexOf(Math.min(...array)),1)
+  array.splice(array.indexOf(Math.max(...array)),1)
+
+  return array.reduce((a,b)=>a+b,0)
+}
+
+
+console.log(sumArray([1,2,3,4,5]))
