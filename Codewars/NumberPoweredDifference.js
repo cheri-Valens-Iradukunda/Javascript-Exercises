42d2145c36b0d55dba9f9aad28c17bb8455279c0
cheri_Valens_Iradukunda <iradukundacherivalens88@gmail.com>
2025-08-19 16:27:01 +0200

    changes

--- Codewars/NumberPoweredDifference.js
+++ Codewars/NumberPoweredDifference.js
@@ -1,7 +1,8 @@
-function differenceOfSquares(n) {
-  let sum1 = Math.pow(Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+b),2)
-  let sum2 = Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+Math.pow(b,2))
-  return sum1-sum2
-}
+// function differenceOfSquares(n) {
+//   let sum1 = Math.pow(Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+b),2)
+//   let sum2 = Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+Math.pow(b,2))
+//   return sum1-sum2
+// }
 
-console.log(differenceOfSquares(10))
+// console.log(differenceOfSquares(10))
+
