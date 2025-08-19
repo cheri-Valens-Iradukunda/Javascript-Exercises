94a19121ad9638376db622969e7c304f3f3ce475
cheri_Valens_Iradukunda <iradukundacherivalens88@gmail.com>
2025-08-19 11:20:41 +0200

    adding another kata

---
+++ Codewars/NumberPoweredDifference.js
@@ -0,0 +1,7 @@
+function differenceOfSquares(n) {
+  let sum1 = Math.pow(Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+b),2)
+  let sum2 = Array.from({length: n}, (_,a)=>a+1).reduce((a,b)=>a+Math.pow(b,2))
+  return sum1-sum2
+}
+
+console.log(differenceOfSquares(10))
