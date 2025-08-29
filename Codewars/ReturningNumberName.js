b15b5beca1f3cad570160c3d09668cf9329eba8d
cheri_Valens_Iradukunda <iradukundacherivalens88@gmail.com>
2025-08-29 10:25:25 +0200

    changes

---
+++ Codewars/ReturningNumberName.js
@@ -0,0 +1,16 @@
+function switchItUp(number){
+//Write your own Code!
+  let numbers = new Map()
+  numbers.set(0,"Zero")
+  numbers.set(1,"One")
+  numbers.set(2,"Two")
+  numbers.set(3,"Three")
+  numbers.set(4,"Four")
+  numbers.set(5,"Five")
+  numbers.set(6,"Six")
+  numbers.set(7,"Seven")
+  numbers.set(8,"Eight")
+  numbers.set(9,"Nine")
+  
+  return numbers.get(number)
+}
